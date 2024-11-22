ebfb3485f8fe87b2c8fa09f27b60001e43ed81a9
Julieee12 <anaiuliaopris96@gmail.com>
2024-11-22 13:08:56 +0100

    added the CRUD files

---
+++ server/DataAccess/Repositories/GameRepository.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories;
+
+public class GameRepository(AppDbContext context) : IGameRepository
+{
+    public List<Game> GetAllGames()
+    {
+        return context.Games.Include(g => g.Boards).ToList();
+    }
+
+    public async Task<Game> AddGame(Game game)
+    {
+        context.Games.Add(game);
+        await context.SaveChangesAsync();
+        return game;
+    }
+
+    public async Task<Game> UpdateGame(Game game)
+    {
+        context.Games.Update(game);
+        await context.SaveChangesAsync();
+        return game;
+    }
+}
