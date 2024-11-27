e1ef6a32d4d36317a88d77f894e7f93b0a39df23
Jean-Jensen <jeanjensen911@gmail.com>
2024-11-28 03:00:12 +0700

    able to see history (user specific if player)

--- client/src/components/admin/AllHistory.tsx
+++ client/src/components/admin/AllHistory.tsx
@@ -3,41 +3,34 @@ import {useEffect, useState} from "react";
 import { http } from "../../http";
 import { BBVenturesApiTransaction } from "../../services/Api";
 
-function History(){
-    
+
+function AllHistory(){
+
     const [allTrans, setAllTrans] = useState<BBVenturesApiTransaction[]>([]);
-    //let a: string | undefined;
-    const [guid] = useState("sdaf7843t7wed")
-    
-    
+
     useEffect(() => {getAllTrans()}, [])
     async function getAllTrans(){
         const response = await http.transactionList();
-        
-        // @ts-ignore
-        const response2 = await http.transactionTransactionsFromUserList(guid);
         setAllTrans(response.data);
-        console.log(response.data)
-        console.log(response2.data)
     }
-    
+
     return <>
-        
+
         <button onClick={getAllTrans}>this is a test button</button>
         {
-            allTrans.map((t) => {return <>
+            allTrans.map((t) => {return <div>
                 <>detected transaction</> <br/>
                 <>Id: {t.id}</> <br/>
                 <>playerId: {t.playerId}</> <br/>
                 <>amount: {t.amount}</> <br/>
                 <br/>
-            </>
+            </div>
             })
         }
-        
+
     </>
-    
-    
+
+
 }
 
-export default History
+export default AllHistory
