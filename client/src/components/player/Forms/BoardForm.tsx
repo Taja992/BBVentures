8a0915ae5f1917d4a694ea6703e92b73500b6341
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-11-28 18:24:29 +0100

    commented out boardform

--- client/src/components/player/Forms/BoardForm.tsx
+++ client/src/components/player/Forms/BoardForm.tsx
@@ -1,4 +1,5 @@
-﻿import React, { useState } from 'react';
+﻿/*
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { http } from '../../../http';
 // import '../playerPage.css';
@@ -49,3 +50,5 @@ const BoardForm = () => {
 };
 
 export default BoardForm;
+    
+ */
