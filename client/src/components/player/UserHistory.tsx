aa91ec47953e835de8b150e48dad0bd02363dc5a
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-12-02 22:26:42 +0100

    Fixed Shit

--- client/src/components/player/UserHistory.tsx
+++ client/src/components/player/UserHistory.tsx
@@ -1,50 +1,39 @@
-
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { http } from "../../http";
 import { BBVenturesApiTransaction } from "../../services/Api";
 
-
-function UserHistory(){
-    
+function UserHistory() {
     const [allTrans, setAllTrans] = useState<BBVenturesApiTransaction[]>([]);
-    
-    useEffect(() => {getAllTrans()}, [])
-    async function getAllTrans(){
-        
+
+    useEffect(() => { getAllTrans() }, []);
+
+    async function getAllTrans() {
         const response = await http.transactionTransactionsFromUserList();
         setAllTrans(response.data);
     }
-    
-    
-    
-    
-    return <>
 
-        <h1 className={"text-2xl font-bold mb-4"}> Your transactions </h1>
-
-        <button onClick={getAllTrans}>this is a test button</button>
-        <br/>
-        {
-            allTrans.map((t) => {
-                return <div>
+    return (
+        <>
+            <h1 className={"text-2xl font-bold mb-4"}> Your transactions </h1>
+            <button onClick={getAllTrans}>this is a test button</button>
+            <br />
+            {allTrans.map((t) => (
+                <div key={t.id}>
                     <>detected transaction</>
-                    <br/>
+                    <br />
                     <>Id: {t.id}</>
-                    <br/>
+                    <br />
                     <>playerId: {t.userId}</>
-                    <br/>
+                    <br />
                     <>amount: {t.amount}</>
-                    <br/>
+                    <br />
                     <>{t.isPending ? "pending" : "approved"}</>
-                    <br/>
-                    <br/>
+                    <br />
+                    <br />
                 </div>
-            })
-        }
-
-    </>
-
-
+            ))}
+        </>
+    );
 }
 
-export default UserHistory
+export default UserHistory;
