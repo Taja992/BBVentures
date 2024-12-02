a8194d3f2744cf6f4cc0e15bdedc94c204c679f4
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-12-02 22:20:45 +0100

    error fixed ✈️ 🗼 🗼

--- client/src/components/player/BoardHistory-UserID-Component.tsx
+++ client/src/components/player/BoardHistory-UserID-Component.tsx
@@ -1,17 +1,16 @@
 ﻿import { useState, useEffect } from 'react';
 import { http } from '../../http';
-import { BBVenturesApiBoardDto, BBVenturesApiGameDto } from '../../services/Api';
+import { BBVenturesApiBoardHistoryDto } from '../../services/Api';
 import toast from 'react-hot-toast';
 
 const BoardHistoryComponent = () => {
-    const [boards, setBoards] = useState<BBVenturesApiBoardDto[]>([]);
-    const [games, setGames] = useState<BBVenturesApiGameDto[]>([]);
+    const [boards, setBoards] = useState<BBVenturesApiBoardHistoryDto[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     const fetchBoards = async () => {
         try {
-            const response = await http.boardUserBoardsList();
+            const response = await http.boardUserBoardHistoryList();
             setBoards(response.data);
         } catch (err) {
             setError('Failed to fetch boards.');
@@ -21,19 +20,8 @@ const BoardHistoryComponent = () => {
         }
     };
 
-    const fetchGames = async () => {
-        try {
-            const response = await http.gameList();
-            setGames(response.data);
-        } catch (err) {
-            setError('Failed to fetch games.');
-            toast.error('Failed to fetch games.');
-        }
-    };
-
     useEffect(() => {
         fetchBoards();
-        fetchGames();
     }, []);
 
     if (loading) {
@@ -43,11 +31,6 @@ const BoardHistoryComponent = () => {
     if (error) {
         return <p className="text-red-500">{error}</p>;
     }
-
-    const getWeekNumber = (gameId: string | undefined) => {
-        const game = games.find(game => game.id === gameId);
-        return game ? game.weekNumber : 'N/A';
-    };
 
     const sortedBoards = [...boards].sort((a, b) => new Date(b.createdAt!).getTime() - new Date(a.createdAt!).getTime());
 
@@ -66,7 +49,7 @@ const BoardHistoryComponent = () => {
                     <tbody>
                     {sortedBoards.map((board) => (
                         <tr key={board.createdAt} className="text-center">
-                            <td className="py-2 px-4 border border-black">{getWeekNumber(board.gameId)}</td>
+                            <td className="py-2 px-4 border border-black">{board.weekNumber}</td>
                             <td className="py-2 px-4 border border-black">{board.numbers?.join(', ') || 'N/A'}</td>
                             <td className="py-2 px-4 border border-black">{board.createdAt ? new Date(board.createdAt).toLocaleString() : 'N/A'}</td>
                         </tr>
