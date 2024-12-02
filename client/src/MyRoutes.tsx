c60f6b45b6767a00c2b1c99c83ecf5f222a0c9ba
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-12-02 18:06:19 +0100

    added additional stuff to get rid of warnings

--- client/src/MyRoutes.tsx
+++ client/src/MyRoutes.tsx
@@ -20,19 +20,29 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
             <Route index element={<LoginPage />} />
-            
+
             <Route element={<PasswordPageLayout />}>
-            <Route path="register-password" element={<RegisterPasswordPage />} />
-            <Route path="request-password" element={<RequestPasswordPage />} />
-            <Route path="reset-password" element={<ResetPasswordPage />} />
+                <Route path="register-password" element={<RegisterPasswordPage />} />
+                <Route path="request-password" element={<RequestPasswordPage />} />
+                <Route path="reset-password" element={<ResetPasswordPage />} />
             </Route>
-            
+
             <Route element={<MainLayout />}>
                 <Route path="test" element={<ComponentTestPage />} />
                 <Route path="dashboard" element={<ProtectedRoute element={Dashboard} />} />
             </Route>
         </Route>
-    )
+    ),
+    {
+        future: {
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+            v7_fetcherPersist: true,
+            v7_normalizeFormMethod: true,
+            v7_partialHydration: true,
+            v7_skipActionErrorRevalidation: true,
+        } as any // Add this type assertion to bypass the type error
+    }
 );
 
 const MyRoutes = () => {
