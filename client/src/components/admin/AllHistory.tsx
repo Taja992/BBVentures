6f4d0a2049e5552b8dfec40cdf2a52924cbe0d1c
Jean-Jensen <jeanjensen911@gmail.com>
2024-12-04 21:52:33 +0700

    made transactions display in a table

--- client/src/components/admin/AllHistory.tsx
+++ client/src/components/admin/AllHistory.tsx
@@ -1,16 +1,23 @@
 import {useEffect, useState} from "react";
 import { http } from "../../http";
-import {BBVenturesApiTransaction} from "../../services/Api";
+import {BBVenturesApiTransaction, BBVenturesApiUser} from "../../services/Api";
 
 
 function AllHistory(){
 
     const [allTrans, setAllTrans] = useState<BBVenturesApiTransaction[]>([]);
+    const [allUsers, setAllUsers] = useState<BBVenturesApiUser[]>([]);
 
-    useEffect(() => {getAllTrans()}, [])
+    useEffect(() => {getAllTrans(); getAllUsers()}, [])
     async function getAllTrans(){
         const response = await http.transactionList();
         setAllTrans(response.data);
+        console.log(response.data);
+    }
+
+    async function getAllUsers(){
+        const response = await http.userGetallList();
+        setAllUsers(response.data);
         console.log(response.data);
     }
     
@@ -34,6 +41,55 @@ function AllHistory(){
         
         //setBalance(newBalance);
     }
+    
+    //below was an attempt at getting the user name via http request but theres not really a way to do it that isnt asynchronous,
+    //which tables dont like. So for now I'm doing the less efficient route of getting all the users at the start and going through
+    //that list
+     /*function getUserNameById(id: string){
+        const response = http.userGetByIdList({id});
+        var playerName;
+        var data;
+        response.then(res => {
+            playerName = res.data.id
+            data = res.data
+            return playerName;
+            //console.log("Id: " + res.data.id);
+        })
+         console.log(data);
+        // console.log("Player Name: " + playerName)
+        return playerName;
+    }*/
+    
+    function getUserNameById(id: string){
+        let name;
+        allUsers.map((user) => {
+            if(user.id === id){
+                name = user.userName;
+            }
+        })
+        
+        if(!name){
+            return "N/A"
+        }
+        
+        return name;
+    }
+
+    function getUserEmailById(id: string){
+        let email;
+        allUsers.map((user) => {
+            if(user.id === id){
+                email = user.email;
+            }
+        })
+
+        if(!email){
+            return "N/A"
+        }
+
+        return email;
+    }
+    
 
     return <>
 
@@ -41,7 +97,43 @@ function AllHistory(){
         
         <button onClick={getAllTrans}>this is a test button</button>
         <br/>
-        {
+        
+        <div className={"table-container"}>
+            <table className={"table-auto bg-white border border-black"}>
+                <thead>
+                <tr>
+                    <th className={"py-2 px-4 border border-black"}>Transaction Amount</th>
+                    <th className={"py-2 px-4 border border-black"}>Player Name</th>
+                    <th className={"py-2 px-4 border border-black"}>Player Email</th>
+                    <th className={"py-2 px-4 border border-black"}>Is Pending</th>
+                    <th className={"py-2 px-4 border border-black"}>Mobile Pay Number</th>
+                    <th className={"py-2 px-4 border border-black"}>Made At</th>
+                </tr>
+                </thead>
+                <tbody>
+                {allTrans.map((trans) => (
+                    <tr key={trans.id} className={"text-center"}>
+                        <td className={"py-2 px-4 border border-black"}> {trans.amount} </td>
+                        <td className={"py-2 px-4 border border-black"}> {getUserNameById(trans.userId!)} </td>
+                        <td className={"py-2 px-4 border border-black"}> {getUserEmailById(trans.userId!)} </td>
+                        <td className={"py-2 px-4 border border-black"}>
+                            <div>
+                                {trans.isPending ? "pending" : "approved"}
+                                {trans.isPending ?
+                                    <button className={"button"}
+                                            onClick={() => approveTransaction(trans)}>approve</button> : <></>}
+                            </div>
+                        </td>
+                        <td className={"py-2 px-4 border border-black"}> {trans.mobilePayTransactionNumber} </td>
+                        <td className={"py-2 px-4 border border-black"}> {trans.createdAt ? new Date(trans.createdAt).toLocaleString() : "N/A"} </td>
+                    </tr>
+                ))}
+                </tbody>
+
+            </table>
+        </div>
+
+        {/*{
             allTrans.map((t) => {
                 return <div>
                     <>detected transaction</>
@@ -59,7 +151,7 @@ function AllHistory(){
                     <br/>
                 </div>
             })
-        }
+        }*/}
 
     </>
 
