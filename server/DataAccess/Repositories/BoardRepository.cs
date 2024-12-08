a7a5b6aec2e3c7d3c36ff5b0b6a787d0657eb48e
thomasbrowne0 <144432350+thomasbrowne0@users.noreply.github.com>
2024-12-08 12:34:18 +0100

    removed comments

--- server/DataAccess/Repositories/BoardRepository.cs
+++ server/DataAccess/Repositories/BoardRepository.cs
@@ -39,7 +39,7 @@ public class BoardRepository(AppDbContext context) : IBoardRepository
         context.SaveChanges();
     }
 
-    public async Task<List<Board>> GetBoardsByUserId(string userId) // New method
+    public async Task<List<Board>> GetBoardsByUserId(string userId)
     {
         return await context.Boards.Where(board => board.UserId == userId).ToListAsync();
     }
