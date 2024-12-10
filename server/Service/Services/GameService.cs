f86afd011543d81a622f2edfd7813a1fc3d70141
Taja992 <Tharaxui@gmail.com>
2024-12-10 16:06:13 +0100

    Added CreatedAt = DateTime.UtcNow, to autoplay boards

--- server/Service/Services/GameService.cs
+++ server/Service/Services/GameService.cs
@@ -278,6 +278,7 @@ public class GameService : IGameService
             GameId = newGameId,
             UserId = board.UserId,
             Numbers = board.Numbers,
+            CreatedAt = DateTime.UtcNow,
             IsAutoplay = true,
             isWon = false
         };
