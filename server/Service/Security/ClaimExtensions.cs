69e16f931e631cf731e7d76f51e2c2079bebb4fd
Taja992 <Tharaxui@gmail.com>
2024-12-16 13:41:03 +0100

    Fixed program.cs crashing(hopefully)

--- server/Service/Security/ClaimExtensions.cs
+++ server/Service/Security/ClaimExtensions.cs
@@ -5,24 +5,24 @@ namespace Service.Security;
 
 public static class ClaimExtensions
 {
-    public static string GetUserId(this ClaimsPrincipal user) =>
-        user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+    // public static string GetUserId(this ClaimsPrincipal user) =>
+    //     user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
 
     public static IEnumerable<Claim> ToClaims(this User identityUser, IEnumerable<string> roles) =>
     [
         new(ClaimTypes.Name, identityUser.UserName!),
-        new(ClaimTypes.NameIdentifier, identityUser.Id.ToString()),
+        new(ClaimTypes.NameIdentifier, identityUser.Id),
         new(ClaimTypes.Email, identityUser.Email!),
         .. roles.Select(role => new Claim(ClaimTypes.Role, role))
     ];
 
-    public static IEnumerable<Claim> ToClaims(this User identityUser, params string[] roles) =>
-        ToClaims(identityUser, roles.AsEnumerable());
-
-    public static ClaimsPrincipal ToPrincipal(this User identityUser, IEnumerable<string> roles) =>
-        new ClaimsPrincipal(new ClaimsIdentity(identityUser.ToClaims(roles)));
-
-    public static ClaimsPrincipal ToPrincipal(this User identityUser, params string[] roles) =>
-        new ClaimsPrincipal(new ClaimsIdentity(identityUser.ToClaims(roles.AsEnumerable())));
+    // public static IEnumerable<Claim> ToClaims(this User identityUser, params string[] roles) =>
+    //     ToClaims(identityUser, roles.AsEnumerable());
+    //
+    // public static ClaimsPrincipal ToPrincipal(this User identityUser, IEnumerable<string> roles) =>
+    //     new ClaimsPrincipal(new ClaimsIdentity(identityUser.ToClaims(roles)));
+    //
+    // public static ClaimsPrincipal ToPrincipal(this User identityUser, params string[] roles) =>
+    //     new ClaimsPrincipal(new ClaimsIdentity(identityUser.ToClaims(roles.AsEnumerable())));
 }
 
