2d507de689ac6d406deeec5841e0a71c93b263fe
thomasbrowne0 <reapple119@gmail.com>
2024-12-19 02:06:21 +0100

    Update BoardValidator.cs

--- server/Service/Validators/BoardValidator.cs
+++ server/Service/Validators/BoardValidator.cs
@@ -7,10 +7,12 @@ namespace Service.Validators
     {
         public BoardValidator()
         {
+            // Ensure that the numbers are unique
             RuleFor(x => x.Numbers)
                 .Must(numbers => numbers.Distinct().Count() == numbers.Count)
                 .WithMessage("Numbers must be unique.");
 
+            // Ensure that the number of selected numbers matches the field count
             RuleFor(x => x.Numbers)
                 .Must((dto, numbers) => numbers.Count == dto.FieldCount)
                 .WithMessage("The number of selected numbers must match the field count.");
